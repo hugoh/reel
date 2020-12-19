ab69f03f5d12d9f0359022c231575e011d813ca4
mattmc3 <mattmc3@gmail.com>
2020-12-19 17:27:52 -0500

    Refactor

--- functions/reel.fish
+++ functions/reel.fish
@@ -1,72 +1,55 @@
 # http://github.com/mattmc3/reel
-# Copyright mattmc3, 2020
-# MIT license
-set -g reel_version 0.0.1
-set -g fish_config ~/.config/fish
-set -g fish_plugins $fish_config/plugins
+# Copyright mattmc3, 2020-2021
+# MIT license, https://opensource.org/licenses/MIT
 
-function reel --description 'The simple, elegant, clutter-free git plugin manager for the fish shell'
-    argparse --stop-nonopt 'v/version' 'h/help' -- $argv
+set -g reel_version 0.1.0
+set -q reel_plugins_path; or set -g reel_plugins_path $HOME/.config/fish/plugins
+set -q reel_git_default_domain; or set -g reel_git_default_domain "github.com"
 
-    if set -q _flag_version
-        printf '%s\n' "reel, version $reel_version"
-    else if set -q _flag_help
-        _reel_usage
-    else if functions --query _reel_cmd_$argv[1]
-        _reel_cmd_$argv[1] $argv[2..-1]
-    else
-        _reel_usage
-        return 1
-    end
-end
-
-function _reel_usage
-    set -l cmd (set_color $fish_color_command)
-    set -l param (set_color $fish_color_param)
+function __reel_usage
     set -l b (set_color --bold)
     set -l n (set_color normal)
 
-    printf '%s\n' \
-        "Usage:$cmd reel$n [options]$param command$n" \
-        "" \
-        "Options:" \
-        "  -v --version  print reel version" \
-        "  -h --help     print this help message" \
-        "" \
-        "Commands:" \
-        "$b  ls$n                 list plugins" \
-        "$b  in$n <plugins...>    initialize a plugin, download if necessary" \
-        "$b  rm$n <plugins...>    remove an existing plugin" \
-        "$b  up$n <plugins...>    update all plugins or just the specified plugin" \
-        "$b  get$n <plugins...>   clone a plugin via git, but don't initialize it"
+    echo "reel - manage your fish plugins"
+    echo ""
+    echo "Comands:"
+    echo "$b ls$n                 List installed plugins"
+    echo "$b in$n <plugins...>    Initialize plugins, cloning from git if necessary"
+    echo "$b rm$n <plugins...>    Remove an existing plugin"
+    echo "$b up$n <plugins...>    Update the specified plugins"
+    echo "$b up$n                 Update all plugins"
+    echo "$b source$n <paths...>  Source a plugin from a directory"
+    echo "$b clone$n <plugin>     git clone the specified plugin"
+    echo ""
+    echo "Options:"
+    echo "       -v or --version  Print version"
+    echo "       -h or --help     Print this help message"
 end
 
-function _reel_error
-    set -l _prefix (set_color --bold brred)'🎣 » error: '(set_color normal)
-    printf '%s' "$_prefix" "$argv" \n
+function __reel_ls
+    for p in $reel_plugins_path/*/*
+        string replace -a "$reel_plugins_path/" "" $p
+    end
 end
 
-function _reel_print
-    set -l _prefix (set_color --bold brgreen)'🎣 » '(set_color normal)
-    printf '%s' "$_prefix" "$argv" \n
+function __reel_up -a plugin
+    if not test -d "$reel_plugins_path/$plugin"
+        echo "plugin not found: $plugin" >&2 && return 1
+    end
+    echo "updating plugin $plugin..."
+    command git -C "$reel_plugins_path/$plugin" pull --recurse-submodules origin
 end
 
-function _reel_warning
-    set -l _prefix (set_color --bold bryellow)'🎣 » warning: '(set_color normal)
-    printf '%s' "$_prefix" "$argv" \n
-end
-
-function _reel_giturl
+function __reel_parse_giturl
     # try to parse a git URL
     set -l parsed (string match -r '^((?:https?|git|ssh):\/\/)(.+)\/([^\/]+)\/([^\/]+?)(?:.git)?$' $argv)
     or set -l parsed (string match -r '^(git@)(.+):([^\/]+)\/([^\/]+?)(?:.git)?$' $argv)
     or set -l parsed (string match -r '^([^\/]+)\/([^\/]+)$' $argv)
 
     if test (count $parsed) -eq 3
-        set parsed "https://github.com/$parsed[2]/$parsed[3]" "https://" "github.com" $parsed[2..-1]
+        set parsed "https://$reel_git_default_domain/$parsed[2]/$parsed[3]" "https://" "$reel_git_default_domain" $parsed[2..-1]
     else if test (count $parsed) -ne 5
-        _reel_error "unable to parse git URL: $argv"
-        return 1
+        echo "unable to parse git URL: $argv" >&2 && return 1
     end
 
     # return the parsed URL as url, protocol, domain, user, and repo
@@ -75,106 +58,94 @@ function _reel_giturl
     end
 end
 
-function _reel_clone
-    _reel_print "cloning repo $$argv[1]"
-    command git clone --recursive --depth 1 $argv[1] "$argv[2]"
+function __reel_clone -a plugin
+    set -l urlparts (__reel_parse_giturl $plugin)
+    if test $status -ne 0
+        echo "invalid plugin: $plugin" >&2 && return 1
+    end
+    set -l plugindir "$reel_plugins_path/$urlparts[4]/$urlparts[5]"
+    if test -d $plugindir
+        echo "plugin already exists" >&2 && return 1
+    end
+    echo "cloning repo $plugin..."
+    command git clone --recursive --depth 1 $urlparts[1] $plugindir
 end
 
-function _reel_cmd_ls
-    for p in $fish_plugins/*/*
-        string replace -a "$fish_plugins/" "" $p
+function __reel_source -a plugin_dir
+    if not test -d "$plugin_dir"
+        echo "plugin not found: $plugin_dir" >&2 && return 1
+    end
+    if test -d "$plugin_dir/completions"; and not contains "$plugin_dir/completions" $fish_complete_path
+        set fish_complete_path "$plugin_dir/completions" $fish_complete_path
+    end
+    if test -d "$plugin_dir/functions"; and not contains "$plugin_dir/functions" $fish_function_path
+        set fish_function_path "$plugin_dir/functions" $fish_function_path
+    end
+    for f in "$plugin_dir/conf.d"/*
+        source "$f"
     end
 end
 
-function _reel_cmd_in
+function __reel_in -a plugin
+    if not test -d "$reel_plugins_path/$plugin"
+        __reel_clone "$plugin"
+    end
+    __reel_source "$reel_plugins_path/$plugin"
+end
+
+function __reel_rm
     if test (count $argv) -eq 0
-        _reel_error "expecting a plugin name"
-        return 1
+        echo "plugin argument expected" >&2 && return 1
     end
-    for p in $argv
-        if not test -d "$fish_plugins/$p"
-            set -l urlparts (_reel_giturl $p)
-            if test $status -ne 0
-                _reel_error "invalid plugin: $p"
-                continue
-            end
-            set -l plugindir "$fish_plugins/$urlparts[4]/$urlparts[5]"
-            if not test -d $plugindir
-                _reel_print "Cloning repo $urlparts[1]"
-                command git clone --recursive --depth 1 $urlparts[1] "$fish_plugins/$urlparts[4]/$urlparts[5]"
-            end
-        end
-        if test -d "$fish_plugins/$p/completions"; and not contains "$fish_plugins/$p/completions" $fish_complete_path
-            set fish_complete_path "$fish_plugins/$p/completions" $fish_complete_path
-        end
-        if test -d "$fish_plugins/$p/functions"; and not contains "$fish_plugins/$p/functions" $fish_function_path
-            set fish_function_path "$fish_plugins/$p/functions" $fish_function_path
-        end
-        for f in "$fish_plugins/$p/conf.d"/*
-            source "$f"
-        end
+    set plugin_path "$reel_plugins_path/$argv"
+    if not test -d "$plugin_path"
+        echo "plugin not found: $argv" >&2 && return 1
+    else if not __reel_can_rm "$plugin_path"
+        echo "plugin path not removable: $argv" >&2 && return 1
+    else
+        echo "removing $plugin_path..."
+        command rm -rf "$plugin_path"
     end
 end
 
-function _reel_cmd_get
-    test (count $argv) -gt 0; or set argv (_reel_cmd_ls)
-    for p in $argv
-        set -l urlparts (_reel_giturl $p); or return 1
-        set -l plugindir "$fish_plugins/$urlparts[4]/$urlparts[5]"
-        if test -d $plugindir
-            _reel_warning "plugin already exists"
-        else
-            _reel_clone $urlparts[1] "$plugindir"
-        end
-    end
-end
-
-function _reel_cmd_up
-    test (count $argv) -gt 0; or set argv (_reel_cmd_ls)
-    for p in $argv
-        if not test -d "$fish_plugins/$p"
-            _reel_error "plugin not found: $p"
-        else
-            _reel_print "updating plugin $p"
-            command git -C "$fish_plugins/$p" pull --recurse-submodules origin
-        end
-    end
-end
-
-function _reel_cmd_rm
-    if test (count $argv) -eq 0
-        while true
-            read -l -P "You are about to remove all your fish plugins. Are you sure [y/n]? " confirm
-            switch $confirm
-                case Y y
-                    set argv (_reel_cmd_ls)
-                    break
-                case '' N n
-                    return 1
-                case
-            end
-        end
-    end
-    for p in $argv
-        set plugin_path (realpath "$fish_plugins/$p")
-        if not test -d "$plugin_path"
-            _reel_error "plugin not found: $p"
-        else if not _reel_is_rm_safe "$plugin_path"
-            _reel_error "plugin path not removable: $p"
-        else
-            _reel_print "removing $plugin_path"
-            command rm -rf "$plugin_path"
-        end
-    end
-end
-
-function _reel_is_rm_safe
+function __reel_can_rm
     # do a small set of checks to make sure someone isn't being evil with
     # relative paths or accidentally gets burnt by a typo
+    set -l abs_pluginsdir (realpath "$reel_plugins_path")
+    set -l abs_pluginpath (realpath "$argv")
+
     set -l path (realpath "$argv")
-    if not string match -q -- "$HOME/*/*" $path
+    if not string match -q -- "$abs_pluginsdir/*/*" "$abs_pluginpath"
         return 1
-    else if not test -d "$path/.git"
+    else if not test -d "$abs_pluginpath/.git"
         return 1
+    end
+end
+
+function reel -a cmd --description 'Manage your fish plugins'
+    set argv $argv[2..-1]
+    switch "$cmd"
+        case -v --version
+            echo "reel, version $reel_version"
+        case "" -h --help
+            __reel_usage
+        case ls list
+            __reel_ls
+        case up update
+            test (count $argv) -gt 0; or set argv (__reel_ls)
+            for p in $argv
+                __reel_up $p
+            end
+        case in init initialize install clone source rm remove
+            if test (count $argv) -eq 0
+                echo "expecting a plugin parameter" >&2 && return 1
+            end
+            contains $cmd install init initialize; and set cmd in
+            contains $cmd remove; and set cmd rm
+            for p in $argv
+                __reel_$cmd $p
+            end
+        case \*
+            echo "reel: Unknown flag or command: \"$cmd\" (see `reel -h`)" >&2 && return 1
     end
 end
