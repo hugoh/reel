af58475712170f226b4402cc15e9fa7d65816482
mattmc3 <mattmc3@gmail.com>
2020-12-20 09:59:08 -0500

    Bumpversion to 1.0.2

--- functions/reel.fish
+++ functions/reel.fish
@@ -2,7 +2,7 @@
 # Copyright mattmc3, 2020-2021
 # MIT license, https://opensource.org/licenses/MIT
 
-set -g reel_version 1.0.1
+set -g reel_version 1.0.2
 set -q reel_plugins_path; or set -g reel_plugins_path $__fish_config_dir/plugins
 set -q reel_git_default_domain; or set -g reel_git_default_domain "github.com"
 
